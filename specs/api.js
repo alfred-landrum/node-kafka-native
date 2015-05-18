d673ba3a9e8c3755868a08e3a7fa820f972c4d21
Alfred Landrum <alfred@jut.io>
2015-05-18 09:05:24 -0700

    test packages

---
+++ specs/api.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+var expect = require('chai').expect;
+var Promise = require('bluebird');
+var jut_node_kafka = require('../index');
+
+describe('api tests', function() {
+    it('should do something', function() {
+        return Promise.resolve();
+    });
+});
